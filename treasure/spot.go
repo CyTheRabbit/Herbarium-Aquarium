1c180adf40903a8ae1fafc36396bd9600e0fdc97
CyTheRabbit <AZOTKirill@gmail.com>
2018-04-23 11:20:21 +0300

    Fancy loot animation

--- treasure/spot.go
+++ treasure/spot.go
@@ -28,6 +28,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "loot"
+  component: "/assets/loot.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
